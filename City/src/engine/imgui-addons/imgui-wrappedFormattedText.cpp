4a206aa5f2a8fa779d01a088c8a69b82b3490f7e
Antonio La Rocca <antoniolarocca95@gmail.com>
2020-11-03 16:45:33 -0500

    * moving my version of wrapped formatted text into a headerfile

---
+++ City/src/engine/imgui-addons/imgui-wrappedFormattedText.cpp
@@ -0,0 +1,166 @@
+#include "stdafx.h"
+
+#include "imgui-wrappedFormattedText.h"
+
+namespace ImGui
+{
+	float ForceDisplayLine(std::string& line, const bool newLine, const bool isColoured, const ImVec4& colour)
+	{
+		if (line == "")
+		{
+			return 0;
+		}
+
+		float lineLength = ImGui::CalcTextSize(line.c_str()).x;
+		if (!newLine)
+		{
+			ImGui::SameLine();
+		}
+
+		if (isColoured)
+		{
+			ImGui::TextColored(colour, line.c_str());
+		}
+		else
+		{
+			ImGui::TextUnformatted(line.c_str());
+		}
+
+		ImGui::SameLine();
+		return lineLength;
+	}
+
+	bool BuildAndDisplayLine(std::string& line, const std::string& token, const bool isColoured, const ImVec4& colour, const float linePos = 0, const float wrap_width = -1)
+	{
+		if (token == "")
+		{
+			return false;
+		}
+
+		std::string newLine(line + (line == "" ? "" : " ") + token);
+		ImVec2 textSize(ImGui::CalcTextSize(newLine.c_str(), (const char*)0, false, wrap_width));
+		float height = ImGui::GetTextLineHeight();
+
+		float lines = textSize.y / height;
+		float actualWidth = textSize.x + linePos;
+		if (lines > 1 || (wrap_width > 0 && actualWidth > wrap_width))
+		{
+			if (isColoured)
+			{
+				ImGui::TextColored(colour, line.c_str());
+			}
+			else
+			{
+				ImGui::TextUnformatted(line.c_str());
+			}
+
+			line = token;
+
+			return true;
+		}
+		else
+		{
+			line = newLine;
+
+			return false;
+		}
+	}
+
+	bool CheckColourAndFormatStart(std::string& testString, ImVec4& outputColour)
+	{
+		if (testString.length() < 8)
+		{
+			return false;
+		}
+
+		if (testString[1] == '#' && testString[0] == '#')
+		{
+			uint32_t colour = std::stol(testString.substr(2, 6), nullptr, 16);
+			testString = testString.substr(8);
+
+			outputColour.x = (colour & 0xFF0000) >> 16;
+			outputColour.y = (colour & 0x00FF00) >> 8;
+			outputColour.z = (colour & 0x0000FF);
+			outputColour.w = 0xFF;
+
+			return true;
+		}
+
+		return false;
+	}
+
+	bool CheckColourAndFormatEnd(std::string& testString)
+	{
+		if (testString.length() < 2)
+		{
+			return false;
+		}
+
+		if (testString[testString.length() - 2] == '$' && testString[testString.length() - 1] == '$')
+		{
+			testString.erase(testString.length() - 2);
+			return true;
+		}
+
+		return false;
+	}	
+
+	void DisplayFormattedText(const std::string& line, float wrap_width)
+	{
+		const char* delimiter = " ";
+
+		size_t next = 0;
+		size_t last = 0;
+
+		std::string token;
+
+		std::string output;
+
+		float startPos = 0;
+		ImVec4 colour;
+
+		bool isColoured = false;
+		bool endColour = false;
+		bool forceLine = false;
+
+		while ((next = line.find(delimiter, last)) != std::string::npos)
+		{
+			token = line.substr(last, next - last);
+			last = next + 1;
+
+			if (CheckColourAndFormatStart(token, colour))
+			{
+				isColoured = true;
+				endColour = false;
+				forceLine = true;
+			}
+
+			if (CheckColourAndFormatEnd(token))
+			{
+				endColour = true;
+			}
+
+			if (forceLine)
+			{
+				startPos += ForceDisplayLine(output, startPos == 0, !isColoured, colour);
+				output = "";
+				forceLine = false;
+			}
+
+			if (BuildAndDisplayLine(output, token, isColoured, colour, startPos, wrap_width))
+			{
+				startPos = 0;
+			}
+
+			if (endColour)
+			{
+				isColoured = false;
+				endColour = false;
+				forceLine = true;
+			}
+		}
+
+		token = line.substr(last);
+		BuildAndDisplayLine(output, token, isColoured, colour, startPos, wrap_width);
+	}
+}
---
+++ City/src/engine/imgui-addons/imgui-wrappedFormattedText.h
@@ -0,0 +1,10 @@
+#pragma once
+#ifndef SRC_IMGUI_WRAPPEDFORMATTEDTEXT_H
+#define SRC_IMGUI_WRAPPEDFORMATTEDTEXT_H
+
+namespace ImGui
+{
+	void DisplayFormattedText(const std::string& line, float wrap_width = -1);
+}
+
+#endif // !SRC_IMGUI_WRAPPEDFORMATTEDTEXT_H
